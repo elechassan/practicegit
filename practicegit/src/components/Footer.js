cf4ab971b6a44762b141fc16977d931886aa2011
hassan elhabouz <elhabouz1987@gmail.com>
2017-05-22 12:07:22 -0400

    added a Footer component

---
+++ practicegit/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <h2>Made by Elsie,David and Hassan</h2>
+       
+      </div>
+    );
+  }
+}
+
+export default Footer;
