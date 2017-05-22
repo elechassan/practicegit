6a6ebe75c10576d927a0dadeb5db7859826551a3
david spencer <dspencer926@gmail.com>
2017-05-22 11:57:20 -0400

    added Picture component

---
+++ practicegit/src/component/Picture.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Picture extends Component {
+  render() {
+    return (
+      <div>
+        <img src='https://media.giphy.com/media/q540XuK9hMxY4/giphy.gif'/>
+      </div>
+    )
+  }
+}
+
+export default Picture;
